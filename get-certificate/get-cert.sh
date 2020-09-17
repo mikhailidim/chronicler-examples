80e44cdfd5f4da748563bc3d7d8a9e540768b310
Michael M <mikhailidim@users.noreply.github.com>
2020-09-17 13:58:26 -0400

    Parameter processing fix
    Added parameter processing and verification

--- get-certificate/get-cert.sh
+++ get-certificate/get-cert.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set +x
 # Set parameters
-hst=$1
-prt=${2:-443}
-
+hst=$1; shift
+if [ $# -ge 1 ]; then
+ prt=${1:-443}
+ shift
+fi
 # Extract Certificate Text
-
-openssl  s_client -connect $hst:$prt $3 </dev/null 2>/dev/null |awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{print $0}'| openssl x509 -noout -text
-
+openssl  s_client -connect $hst:$prt $@ 2>/dev/null </dev/null  |awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{print $0}'| openssl x509 -noout -text
 
