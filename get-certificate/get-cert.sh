352d60e87675f564cdc5e5495bd2ab14f6cca2a1
Michael M <mikhailidim@users.noreply.github.com>
2020-09-17 21:32:31 -0400

    Handle empty port number

--- get-certificate/get-cert.sh
+++ get-certificate/get-cert.sh
@@ -5,7 +5,10 @@ hst=$1; shift
 if [ $# -ge 1 ]; then
  prt=${1:-443}
  shift
+else
+ prt=443 
 fi
 # Extract Certificate Text
-openssl  s_client -connect $hst:$prt $@ 2>/dev/null </dev/null  |awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{print $0}'| openssl x509 -noout -text
-
+openssl  s_client -connect $hst:$prt $@ 2>/dev/null </dev/null  |\
+ awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{print $0}'|\
+ openssl x509 -noout -text 
